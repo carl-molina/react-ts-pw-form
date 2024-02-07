ce29327b154255f4aca90b6b46b7556f9699b6d1
Carl Molina <carlandrew.molina@gmail.com>
2024-02-06 21:07:00 -0800

    Moved interface out, added console.log, removed old code

--- src/PasswordForm.tsx
+++ src/PasswordForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { IPasswordFormData } from "./interfaces";
+import { IPasswordFormData, IPasswordFormProps } from "./interfaces";
 
 
 /** PasswordForm: checks whether a password is valid password.
@@ -15,18 +15,18 @@ import { IPasswordFormData } from "./interfaces";
  *  App -> PasswordForm
  */
 
-interface IPasswordFormProps {
-    initialFormData?: IPasswordFormData;
-    handleSave: (formData: IPasswordFormData) => void;
-}
-
-const defaultInitialFormData: IPasswordFormData = {newPw: "", confirmNewPw: ""};
+const defaultInitialFormData: IPasswordFormData = {
+    newPw: "",
+    confirmNewPw: ""
+};
 
 function PasswordForm({ initialFormData = defaultInitialFormData, handleSave }
-        : IPasswordFormProps) {
+    : IPasswordFormProps) {
 
     const [formData, setFormData] = useState(initialFormData);
     const [errs, setErrs] = useState([]);
+
+    console.log('PasswordForm formData: ', formData);
 
     /** Update form input */
     function handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
@@ -46,98 +46,77 @@ function PasswordForm({ initialFormData = defaultInitialFormData, handleSave }
     }
 
 
+    // attribution for regex pattern for password:
+    // https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    const REGEX_PW = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+    console.log('This is REGEX_PW: ', REGEX_PW);
+
+
+
+
+
     return (
-        <form>
-            <div className="PasswordForm-header">
-                <h1>Welcome back CARL!</h1>
-                <p>Your username is CARLMOLINA42</p>
-                <br/>
-                <p>Create a new password.</p>
-                <p>Make sure it does not match your prior passwords.</p>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-12">
+                    <div className="card mt-5">
+                        <div className="card-body">
+                            <h1 className="card-title text-center">
+                                Welcome back CARL!
+                            </h1>
+                            <h6>Your username is CARLMOLINA42</h6>
+                            <form
+                                className="PasswordForm-form"
+                                onSubmit={handleSubmit}
+                            >
+                                <div className="form-group mt-5 mb-5">
+                                    <h6>Create a new password.</h6>
+                                    <h6>
+                                        Make sure it does not match your prior
+                                        passwords.
+                                    </h6>
+                                </div>
+                                <div className="form-group mb-5">
+                                    <label htmlFor="newPw">
+                                        Enter new password *
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="newPw"
+                                        name="newPw"
+                                        onChange={handleChange}
+                                        value={formData.newPw}
+                                        required
+                                        pattern={REGEX_PW}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="confirmNewPw">
+                                        Confirm password *
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="confirmNewPw"
+                                        name="confirmNewPw"
+                                        onChange={handleChange}
+                                        value={formData.confirmNewPw}
+                                        required
+                                    />
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary btn-block mt-5">
+                                    Reset Password
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="PasswordForm-newPw">
-                <label htmlFor="PasswordForm-newPw">New Password: </label>
-                <input
-                    type="password"
-                    id="PasswordForm-newPw"
-                    name="newPw"
-                    placeholder="new password"
-                    onChange={handleChange}
-                    value={formData.newPw}
-                    required
-                />
-            </div>
-            <div className="PasswordForm-confirmNewPw">
-                <label htmlFor="PasswordForm-confirmNewPw">Confirm New Password: </label>
-                <input
-                    type="password"
-                    id="PasswordForm-confirmNewPw"
-                    name="confirmNewPw"
-                    placeholder="new password"
-                    onChange={handleChange}
-                    value={formData.confirmNewPw}
-                    required
-                />
-            </div>
-
-            <button className="PasswordForm-btn">SUBMIT NEW CHANGES</button>
-
-        </form>
-    )
-
-
+        </div>
+    );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// older code for reference:
-
-// type State = {
-//     text: string;
-// };
-
-// class PasswordForm extends React.Component<Props, State> {
-//     state = {
-//         text: "",
-//     };
-
-//     onChange = (e: React.FormEvent<HTMLInputElement>): void => {
-//         this.setState({ text: e.currentTarget.value });
-//     };
-
-//     render() {
-//         return (
-//             <div>
-//                 <input type="text" value={this.state.text} onChange={this.onChange} />
-//             </div>
-//         )
-//     }
-// }
-
-
-// oldest code for reference:
-
-// function PasswordForm() {
-//     const [formData, setFormData] = useState("");
-//     const [errs, setErrs] = useState([]);
-
-
-
-//     return (
-//         <>
-
-//         </>
-//     )
-// }
-
 
 export default PasswordForm;
