6ec9cdc0e94fffbb5e6d67fcb084bfef2f94cbf4
Carl Molina <carlandrew.molina@gmail.com>
2024-02-08 20:14:11 -0800

    Added initial tests for app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,30 +1,38 @@
 import React from "react";
-import { test, expect } from "vitest";
-import { render, screen, fireEvent } from '@testing-library/react';
+import { test, expect, describe } from "vitest";
+import { render } from '@testing-library/react';
 
 import App from './App.js';
 
 
-// test('renders counter button', function () {
-//   render(<App />);
-//   const buttonElement = screen.getByText(/Clicked: 0/i);
-//   expect(buttonElement).toBeInTheDocument();
-// });
 
-// test('count increases on button click', function () {
-//   render(<App />);
-//   const buttonElement = screen.getByText(/Clicked: 0/i);
-//   fireEvent.click(buttonElement);
-//   expect(buttonElement).toHaveTextContent(/Clicked: 1/i);
-// });
-
-
-test('renders without crashing', function () {
-  const res = render(<App />);
-  expect(res.queryByText('Welcome back CARL!')).toBeInTheDocument();
-})
-
-test('matches snapshot', function () {
-  const { container } = render(<App />);
-  expect(container).toMatchSnapshot();
+describe("App smoke test", function () {
+  test('renders without crashing', function () {
+    render(<App />);
+  });
 });
+
+
+describe("App renders correct data", function () {
+  test('contains expected text', function () {
+    const res = render(<App />);
+    expect(res.queryByText('Welcome back CARL!')).toBeInTheDocument();
+    expect(res.queryByText('❌ Include a number')).toBeInTheDocument();
+    expect(res.queryByText('❌ Password must match')).toBeInTheDocument();
+  });
+
+  test('button attribute set to disabled on initial render', function () {
+    const { container, debug } = render(<App />);
+    const btn = container.querySelector('button')!;
+    expect(btn.getAttribute('disabled')).toBeDefined();
+  });
+});
+
+
+
+describe("App snapshot test", function () {
+  test('matches snapshot', function () {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+})
