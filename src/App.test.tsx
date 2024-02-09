184eff3f6c751352340aa43559d6970df08c6819
Carl Molina <carlandrew.molina@gmail.com>
2024-02-08 19:36:49 -0800

    Added initial testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,18 +5,24 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App.js';
 
 
-test('renders counter button', function () {
-  render(<App />);
-  const buttonElement = screen.getByText(/Clicked: 0/i);
-  expect(buttonElement).toBeInTheDocument();
-});
+// test('renders counter button', function () {
+//   render(<App />);
+//   const buttonElement = screen.getByText(/Clicked: 0/i);
+//   expect(buttonElement).toBeInTheDocument();
+// });
 
-test('count increases on button click', function () {
-  render(<App />);
-  const buttonElement = screen.getByText(/Clicked: 0/i);
-  fireEvent.click(buttonElement);
-  expect(buttonElement).toHaveTextContent(/Clicked: 1/i);
-});
+// test('count increases on button click', function () {
+//   render(<App />);
+//   const buttonElement = screen.getByText(/Clicked: 0/i);
+//   fireEvent.click(buttonElement);
+//   expect(buttonElement).toHaveTextContent(/Clicked: 1/i);
+// });
+
+
+test('renders without crashing', function () {
+  const res = render(<App />);
+  expect(res.queryByText('Welcome back CARL!')).toBeInTheDocument();
+})
 
 test('matches snapshot', function () {
   const { container } = render(<App />);
